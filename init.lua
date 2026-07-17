a34c627ca210b4cfcaf5aef237c82cf9cde725f3
Quinn <230894588+papawhomaomao-rgb@users.noreply.github.com>
2026-07-17 15:32:28 -0700

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,106 @@
+--!nocheck
+local license = ... or {}
+license.Key = script_key or license.Key
+
+local cloneref = cloneref or function(ref) return ref end
+local isfile = isfile or function(file)
+	local suc, res = pcall(function()
+		return readfile(file)
+	end)
+	return suc and res ~= nil and res ~= ''
+end
+local delfile = delfile or function(file)
+	writefile(file, '')
+end
+
+local downloader = Instance.new('TextLabel')
+downloader.Size = UDim2.new(1, 0, 0, 40)
+downloader.BackgroundTransparency = 1
+downloader.TextStrokeTransparency = 0
+downloader.TextSize = 20
+downloader.TextColor3 = Color3.new(1, 1, 1)
+downloader.Font = Enum.Font.Arial
+downloader.Text = ''
+downloader.Parent = Instance.new('ScreenGui', gethui and gethui() or cloneref(game:GetService('CoreGui')))
+
+local function downloadFile(path, func)
+	if not isfile(path) then
+		if not license.Closet then
+			downloader.Text = 'Downloading '.. path
+		end
+		local suc, res = pcall(function()
+			return game:HttpGet('https://raw.githubusercontent.com/MaxlaserTech/CatV6/'..readfile('catrewrite/profiles/commit.txt')..'/'..select(1, path:gsub('catrewrite/', '')), true)
+		end)
+		if not suc or res == '404: Not Found' then
+			error(res)
+		end
+		if path:find('.lua') then
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+		end
+		writefile(path, res)
+		downloader.Text = ''
+	end
+	return (func or readfile)(path)
+end
+
+local function wipeFolder(path)
+	if not isfolder(path) then return end
+	for _, file in listfiles(path) do
+		if file:find('init') then continue end
+		if file:find('profile') then continue end
+		if isfile(file) then
+			delfile(file)
+		elseif isfolder(file) then
+			wipeFolder(file)
+		end
+	end
+end
+
+
+for _, folder in {'catrewrite', 'catrewrite/games', 'catrewrite/profiles', 'catrewrite/assets', 'catrewrite/libraries', 'catrewrite/guis'} do
+	if not isfolder(folder) then
+		downloader.Text = 'Downloading '.. folder
+		makefolder(folder)
+	end
+end
+
+if not shared.VapeDeveloper then
+	local commit = license.Commit or nil
+	if not commit then
+		local _, subbed = pcall(function() 
+			return game:HttpGet('https://github.com/MaxlaserTech/CatV6') 
+		end)
+		commit = subbed:find('currentOid')
+		commit = commit and subbed:sub(commit + 13, commit + 52) or nil
+		commit = commit and #commit == 40 and commit or 'main'
+	end
+	if commit == 'main' or (isfile('catrewrite/profiles/commit.txt') and readfile('catrewrite/profiles/commit.txt') or '') ~= commit then
+		if commit ~= 'main' and isfile('catrewrite/profiles/commit.txt') then
+			shared.updated = readfile('catrewrite/profiles/commit.txt')
+		end
+		wipeFolder('catrewrite')
+		wipeFolder('catrewrite/games')
+		wipeFolder('catrewrite/guis')
+		wipeFolder('catrewrite/libraries')
+	end
+	writefile('catrewrite/profiles/commit.txt', commit)
+	if #listfiles('catrewrite/profiles') < 4 then
+		local req = request({
+			Url = 'https://api.github.com/repos/maxlasertech/catv6/contents/profiles',
+			Method = 'GET'
+		})
+		if req.StatusCode == 200 then
+			local body = cloneref(game:GetService('HttpService')):JSONDecode(req.Body)
+			if body and typeof(body) == 'table' then
+				for _, v in body do
+					if v.type == 'file' then
+						pcall(downloadFile, 'catrewrite/'.. ({v.path:gsub(' ', '%%20')})[1])
+					end
+				end
+			end
+		end
+	end
+end
+
+downloader.Text = ''
+return loadstring(downloadFile('catrewrite/main.lua'), 'main')(license)
---
+++ main.lua
@@ -0,0 +1,141 @@
+local license = ... or {}
+license.Key = script_key or license.Key or '_key'
+getgenv().license = license
+repeat task.wait() until game:IsLoaded()
+if shared.vape then shared.vape:Uninject() end
+
+local vape
+local loadstring = function(...)
+	local res, err = loadstring(...)
+	if err and vape then
+		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
+	end
+	return res
+end
+local queue_on_teleport = queue_on_teleport or function() end
+local isfile = isfile or function(file)
+	local suc, res = pcall(function()
+		return readfile(file)
+	end)
+	return suc and res ~= nil and res ~= ''
+end
+local cloneref = cloneref or function(obj)
+	return obj
+end
+local playersService = cloneref(game:GetService('Players'))
+local httpService = cloneref(game:GetService('HttpService'))
+
+local function downloadFile(path, func)
+	if not isfile(path) then
+		local suc, res = pcall(function()
+			return game:HttpGet('https://raw.githubusercontent.com/MaxlaserTech/CatV6/'..readfile('catrewrite/profiles/commit.txt')..'/'..select(1, path:gsub('catrewrite/', '')), true)
+		end)
+		if not suc or res == '404: Not Found' then
+			task.spawn(error, res)
+		end
+		if suc then
+			if path:find('.lua') then
+				res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+			end
+			writefile(path, res)
+		end
+	end
+	return (func or readfile)(path)
+end
+
+local function finishLoading()
+	vape.Init = nil
+	vape:Load()
+	task.spawn(function()
+		repeat
+			vape:Save()
+			task.wait(10)
+		until not vape.Loaded
+	end)
+
+	local teleportedServers
+	vape:Clean(playersService.LocalPlayer.OnTeleport:Connect(function(state)
+		if (not teleportedServers) and (not shared.VapeIndependent) then
+			teleportedServers = true
+			local teleportScript = [[
+				shared.vapereload = true
+				if shared.VapeDeveloper then
+					loadstring(readfile('catrewrite/main.lua'), 'main')(_scriptconfig)
+				else
+					loadstring(readfile('catrewrite/main.lua'), 'main')(_scriptconfig)
+				end
+			]]
+			local teleportConfig = httpService:JSONEncode(license)
+			teleportConfig = teleportConfig:gsub('":true', "=true"):gsub('{"', '{')
+			teleportConfig = teleportConfig:gsub(',"', ','):gsub('":', '=')
+			teleportConfig = teleportConfig:gsub('%[', '{'):gsub('%]', '}')
+			teleportScript = teleportScript:gsub('_key', tostring(license.Key or '_key'))
+			teleportScript = teleportScript:gsub('_scriptconfig', teleportConfig)
+			if identifyexecutor() == 'Potassium' then
+				teleportScript = 'task.wait(12)\n'.. teleportScript
+			end
+			if shared.VapeDeveloper then
+				teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
+			end
+			if shared.VapeCustomProfile then
+				teleportScript = 'shared.VapeCustomProfile = "'..shared.VapeCustomProfile..'"\n'..teleportScript
+			end
+			queue_on_teleport(teleportScript)
+		end
+	end))
+
+	if not vape.Categories then return end
+	if vape.Categories.Main.Options['GUI bind indicator'].Enabled then
+		if getgenv().catrole == 'HWID MISMATCH' then
+			vape:CreateNotification('Cat', 'HWID MISMATCH, Go to the script panel to reset hwid', 25, 'alert')
+			getgenv().catrole = ''
+			task.wait(0.1)
+		end
+		if not shared.vapereload then
+			vape:CreateNotification('Finished Loading', (getgenv().catname and `Authenticated as {getgenv().catname} with {getgenv().catrole}, ` or '').. (vape.VapeButton and 'Press the button in the top right' or 'Press '..table.concat(vape.Keybind, ' + '):upper())..' to open GUI', 5)
+			task.delay(0.05 + cloneref(game:GetService('RunService')).PostSimulation:Wait(), function()
+				if shared.updated then
+					vape:CreateNotification('Cat', `Script has updated from {shared.updated} to {readfile('catrewrite/profiles/commit.txt')}`, 10, 'info')
+				end
+			end)
+		end
+	end
+end
+
+if not isfile('catrewrite/profiles/gui.txt') then
+	writefile('catrewrite/profiles/gui.txt', 'new')
+end
+local gui = 'new'--readfile('catrewrite/profiles/gui.txt')
+
+if not isfolder('catrewrite/assets/'..gui) then
+	makefolder('catrewrite/assets/'..gui)
+end
+if not isfile('catrewrite/profiles/commit.txt') then
+	writefile('catrewrite/profiles/commit.txt', 'main')
+end
+
+getgenv().used_init = true
+vape = loadstring(downloadFile('catrewrite/guis/'..gui..'.lua'), 'gui')(license)
+_G.vape = vape
+shared.vape = vape
+
+if not shared.VapeIndependent then
+	loadstring(downloadFile('catrewrite/games/universal.lua'), 'universal')(license)
+	if isfile('catrewrite/games/'..game.PlaceId..'.lua') then
+		loadstring(readfile('catrewrite/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(license)
+	else
+		if not shared.VapeDeveloper then
+			local suc, res = pcall(function()
+				return game:HttpGet('https://raw.githubusercontent.com/MaxlaserTech/CatV6/'..readfile('catrewrite/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
+			end)
+			if suc and res ~= '404: Not Found' then
+				loadstring(downloadFile('catrewrite/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(license)
+			end
+		end
+	end
+	loadstring(downloadFile('catrewrite/libraries/premium.lua'), 'premium')(license)
+	finishLoading()
+else
+	vape.Init = finishLoading
+	return vape
+end
